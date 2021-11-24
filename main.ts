ef16c8358a116f1faac6152b63e62a1bcd2d4d3a
aarnavvj <81868372+aarnavvj@users.noreply.github.com>
2021-11-24 12:13:27 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,14 @@
-let Paddle: game.LedSprite = null
 input.onButtonPressed(Button.A, function () {
     Paddle.change(LedSpriteProperty.X, -1)
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    game.pause()
 })
 input.onButtonPressed(Button.B, function () {
     Paddle.change(LedSpriteProperty.X, 1)
 })
+let Paddle: game.LedSprite = null
+game.resume()
 basic.forever(function () {
     let Line: game.LedSprite = null
     let Ball: game.LedSprite = null
